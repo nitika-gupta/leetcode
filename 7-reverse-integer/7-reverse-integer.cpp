19459d8c2a26fcf0ee72e9a517ba9eda2ad0aa99
nitika-gupta <ngnitika1@gmail.com>
2022-02-28 12:16:14 +0530

    Time: 3 ms (59.01%), Space: 5.9 MB (77.32%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x) {
+        //int temp =abs(x);
+        int sum=0;
+        
+        while(x!=0){
+            int rem=x% 10;
+            x=x/10;
+            if(sum>INT_MAX/10 || (sum==INT_MAX/10 && rem>7)) return 0;
+            if(sum<INT_MIN/10 || (sum==INT_MIN/10 && rem<-8)) return 0;
+            sum=sum*10 +rem;
+            
+        }
+        
+      
+        
+        
+        return sum;
+    }
+};
