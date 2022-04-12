a8925a980ca369f2b35d42384448da9378488360
nitika-gupta <ngnitika1@gmail.com>
2022-04-12 14:11:00 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -36,7 +36,22 @@ class Solution
     { 
        // Your code here
        vector<vector<int>> dp(n+1,vector<int>(W+1,-1));
-       return solve(n-1,W,wt,val,n,dp);
+       for(int i=0;i<=n;i++){
+           for(int j=0;j<=W;j++){
+               if(i==0 || j==0)
+               dp[i][j]=0;
+           }
+       }
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=W;j++){
+               if(wt[i-1]<=j)
+               dp[i][j]= max(val[i-1] +dp[i-1][j-wt[i-1]], dp[i-1][j]);
+               else
+               dp[i][j]=dp[i-1][j];
+           }
+       }
+       return dp[n][W];
+      // return solve(n-1,W,wt,val,n,dp);
     }
 };
 
