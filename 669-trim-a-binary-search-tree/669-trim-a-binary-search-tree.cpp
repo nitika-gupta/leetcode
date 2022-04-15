b1cda6c0a4cade03af6f749c089c81d1fe207d9c
nitika-gupta <ngnitika1@gmail.com>
2022-04-15 16:05:38 +0530

    Time: 20 ms (61.23%), Space: 24 MB (7.08%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -17,14 +17,14 @@ public:
         {
             return NULL;
         }
-        if(root->val>=low && root->val<=high)
+        if(root->val<low)
+        {
+            root=trimBST(root->right,low,high);
+        }
+        else if(root->val>=low && root->val<=high)
         {
             root->left=trimBST(root->left,low,high);
             root->right=trimBST(root->right,low,high);
-        }
-        else if(root->val<low)
-        {
-            root=trimBST(root->right,low,high);
         }
         else if(root->val>high)
         {
