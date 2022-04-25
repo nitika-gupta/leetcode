117672bd411961f6a8f379a0c31116e74284f357
nitika-gupta <ngnitika1@gmail.com>
2022-04-25 19:53:17 +0530

    Time: 19 ms (20.12%), Space: 17.3 MB (86.01%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -11,25 +11,77 @@
  */
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* root) {
-         if(root == NULL) return 0;
-        int res = 0;
-		queue<pair<TreeNode*, unsigned int>> q;        // queue<Nodes, Indexes of the Nodes> q
-        q.push({root, 0});
-
-        while(!q.empty()){
-        	auto size = q.size();
-			unsigned int leftNodeIdx = q.front().second;     // Here, idx can't go negative. Therefore, I've used Unsigned Int
-        	unsigned int rightNodeIdx = 0;
-        	while(size--){
-        		auto [node, rightIdx] = q.front();	q.pop();
-        		rightNodeIdx = rightIdx;
-
-        		if(node->left != NULL) q.push({node->left, 2 * rightNodeIdx + 1});
-        		if(node->right != NULL) q.push({node->right, 2 * rightNodeIdx + 2});
-        	}
-        	res = max(res, int(rightNodeIdx - leftNodeIdx + 1));
-        }
-        return res;
+int widthOfBinaryTree(TreeNode* root) 
+    {
+    //     if(root==NULL)
+    //     {
+    //         return 0;
+    //     }
+    //     int ans=0;
+    //     queue<pair<TreeNode*,int>>q;
+    //     q.push({root,0});
+    //     while(!q.empty())
+    //     {
+    //         int n=q.size();
+    //         int p=q.front().second;
+    //        long long int first ,last;
+    //         for(int i=0;i<n;i++)
+    //         {
+    //             int cur_index=q.front().second-p;
+    //             TreeNode* node=q.front().first;
+    //             q.pop();
+    //             if(i==0)
+    //             {
+    //                 first=cur_index;
+    //             }
+    //             if(i==n-1)
+    //             {
+    //                 last=cur_index;
+    //             }
+    //             if(node->left!=NULL)
+    //             {
+    //                 q.push({node->left,cur_index*2+1});
+    //             }
+    //             if(node->right!=NULL)
+    //             {
+    //                 q.push({node->right,cur_index*2+2});
+    //             }
+    //         }
+    //         ans=max(ans,last-first+1);
+    //     }
+    //     return ans;
+    // }
+    if (!root)
+    return 0;
+ long long int ans = 0;
+  queue < pair < TreeNode * ,long long int >> q;
+  q.push({
+    root,
+    0
+  });
+  while (!q.empty()) {
+   long long int size = q.size();
+   long long int curMin = q.front().second;
+    long long  int leftMost, rightMost;
+    for (int i = 0; i < size; i++) {
+     long long int cur_id = q.front().second - curMin; // subtracted to prevent integer overflow
+      TreeNode * temp = q.front().first;
+      q.pop();
+      if (i == 0) leftMost = cur_id;
+      if (i == size - 1) rightMost = cur_id;
+      if (temp -> left)
+        q.push({
+          temp -> left,
+          cur_id * 2 + 1
+        });
+      if (temp -> right)
+        q.push({
+          temp -> right,
+          cur_id * 2 + 2
+        });
     }
+    ans = max(ans, rightMost - leftMost + 1);
+  }
+  return ans;
+}
 };
