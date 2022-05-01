2bf639e2dfc961deb1625e4d228510831055d3b1
nitika-gupta <ngnitika1@gmail.com>
2022-05-01 23:14:17 +0530

    Time: 5 ms (18.70%), Space: 6.5 MB (26.06%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,38 +1,121 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> s1,s2;
-        for(int i=0;i<s.length();i++){
-            if(s[i]!='#')
-                s1.push(s[i]);
-            else if(!s1.empty()){
-                s1.pop();
+      string s1="";
+        string s2="";
+//         int n=s.size();
+//         int m=t.size();
+//         int c=0;int c1=0;
+//         int i=n-1;int f=0;
+//         while(i>=0)
+//         {
+//             if(s[i]=='#')
+//             {
+//                 c++;
+//                 if(s[i-1]!='#')
+//                 {
+//                     i=i-2;
+//                     // if(i<=-1)
+//                     // {
+//                     //     f=1;
+//                     //     break;
+//                     //     //i=-1;
+//                     // }
+//                     c--;
+//                 }
+//             }
+//             else
+//             {
+//                 if(c==0)
+//                 {
+//                     s1+=s[i];
+//                 }
+//                 else
+//                 {
+//                     c--;
+//                 }
+//             }
+//             i--;
+//         }
+//         i=m-1;int g=0;
+//         while(i>=0)
+//         {
+//             if(t[i]=='#')
+//             {
+//                 c1++;
+//                 if(t[i-1]!='#')
+//                 {
+                    
+//                     i=i-2;
+//                     // if(i<=-1)
+//                     // {
+//                     //     g=1;
+//                     //     break;
+//                     // }
+                    
+//                     c1--;
+//                 }
+//             }
+//             else
+//             {
+//                 if(c1==0)
+//                 {
+//                     s2+=t[i];
+//                 }
+//                 else
+//                 {
+//                     c1--;
+//                 }
+//             }
+//             i--;
+//         }
+//         cout<<s1<<endl;
+//         cout<<s2<<endl;
+//         if(s1.size()==0 && s2.size()==0){
+//            // cout<<"x";
+//             return true;
+//         }
+            
+//         if(s1==s2)
+//         {
+//             return true;
+//         }
+//         return false;
+        
+        int c1=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]=='#'){
+                c1++;
+            }
+            else{
+                if(c1>0){
+                    c1--;
+                    continue;
+                    
+                }
+                if(c1==0){
+                    s1=s[i]+s1;
+                }
             }
         }
-        
-        string temp1="";
-        while(!s1.empty()){
-            temp1+=s1.top();
-            s1.pop();
-        }
-        reverse(temp1.begin(),temp1.end());
-        
-        for(int i=0;i<t.length();i++){
-            if(t[i]!='#')
-                s2.push(t[i]);
-            else if(!s2.empty()){
-                s2.pop();
+         int c2=0;
+        for(int i=t.length()-1;i>=0;i--){
+            if(t[i]=='#'){
+                c2++;
+            }
+            else{
+                if(c2>0){
+                    c2--;
+                    continue;
+                    
+                }
+                if(c2==0){
+                    s2=t[i]+s2;
+                }
             }
         }
-         string temp2="";
-        while(!s2.empty()){
-            temp2+=s2.top();
-            s2.pop();
-        }
-        reverse(temp2.begin(),temp2.end());
-        if(temp1==temp2)
-            return true;
-        return false;
-        
+        cout<<s1<<endl;
+        cout<<s2<<endl;
+        return (s1==s2);
     }
 };
