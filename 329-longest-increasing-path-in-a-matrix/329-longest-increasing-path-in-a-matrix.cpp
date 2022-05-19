d2d360f56b68654258379c2821c196b0faecf1c0
nitika-gupta <ngnitika1@gmail.com>
2022-05-19 15:07:00 +0530

    Time: 134 ms (20.99%), Space: 17.1 MB (32.09%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    int dfs(int i,int j , vector<vector<int>>& matrix,int c,int &maxp, vector<vector<int>>& dp){
+    int dfs(int i,int j , vector<vector<int>>& matrix, vector<vector<int>>& dp){
         if(i<0 || j<0 || i>=matrix.size()|| j>=matrix[0].size()  )
-            return c;
+            return 0;
         
         if(dp[i][j]!=-1)
             return dp[i][j];
         int x=0,y=0,z=0,w=0;
       
         if(i-1>=0 && matrix[i][j]<matrix[i-1][j] ){
-           
-            //cout<<c<<" ";
-            maxp=max(c+1,maxp);
-            cout<<maxp<<" ";
-           x=dfs(i-1,j,matrix,c+1,maxp,dp);
+      
+           x=dfs(i-1,j,matrix,dp);
       
         }
          if(j-1>=0 && matrix[i][j]<matrix[i][j-1] ){
 
-            maxp=max(c+1,maxp);
-            y =dfs(i,j-1,matrix,c+1,maxp,dp);
+            y =dfs(i,j-1,matrix,dp);
    
         }
          if(i+1<matrix.size() && matrix[i][j]<matrix[i+1][j]){
       
-            maxp=max(c+1,maxp);
-           z= dfs(i+1,j,matrix,c+1,maxp,dp);
+           z= dfs(i+1,j,matrix,dp);
     
         }
          if(j+1<matrix[0].size() && matrix[i][j]<matrix[i][j+1] ){
 
-            maxp=max(c+1,maxp);
-            w=dfs(i,j+1,matrix,c+1,maxp,dp);
+            w=dfs(i,j+1,matrix,dp);
      
         }
-        cout<<maxp<<" ";
+      
         return dp[i][j]=1 + max({x,y,z,w});
         
         
@@ -49,9 +43,16 @@ public:
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
               
-                maxp=max(maxp,dfs(i,j,matrix,1,maxp,dp));
+                maxp=max(maxp,dfs(i,j,matrix,dp));
             }
         }
+        for(int i=0;i<dp.size();i++){
+            for(int j=0;j<dp[0].size();j++){
+                cout<<dp[i][j];
+            }
+            cout<<endl;
+        }
+        
         return maxp;
     }
 };
