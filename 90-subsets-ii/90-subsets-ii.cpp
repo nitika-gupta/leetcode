49ba45f0ad54e1dc0a7e1243d5fa0d46c23c3b6b
nitika-gupta <ngnitika1@gmail.com>
2022-06-30 14:07:18 +0530

    Time: 27 ms (5.08%), Space: 18 MB (7.10%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -3,7 +3,7 @@ public:
     
     void f(vector<int> & nums, int i, vector<int> v,map<vector<int>,int> & mp){
         if(i==nums.size()){
-            sort(v.begin(),v.end());
+            //sort(v.begin(),v.end());
             mp.insert({v,1});
             return;
         }
@@ -17,6 +17,7 @@ public:
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> v;
+        sort(nums.begin(),nums.end());
         map<vector<int>,int> mp;
         f(nums,0,v,mp);
         vector<vector<int>> res;
