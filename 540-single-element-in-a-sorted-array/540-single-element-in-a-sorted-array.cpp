f2371ec2c88d29601416a287f969813efd7299e2
nitika-gupta <ngnitika1@gmail.com>
2022-07-02 19:32:03 +0530

    Time: 58 ms (12.64%), Space: 22.4 MB (54.50%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int search(int l, int h, vector<int> & nums){
+        if(l>h)
+            return -1;
+        if(l==h){
+            
+        }
+        
+        int mid= l+(h-l)/2;
+        if(mid==0){
+            if(nums[mid+1]!=nums[mid])
+                return nums[mid];
+        }
+        if(mid==nums.size()-1){
+            if(nums[mid-1]!=nums[mid])
+                return nums[mid];
+        }
+        int flag1=0,flag2=0;
+        if(mid!=0 ){
+            if(nums[mid-1]!= nums[mid])
+                flag1=1;
+        }
+        if(mid!=nums.size()-1)
+            if( nums[mid+1]!=nums[mid]){
+                 flag2=1;
+        }
+        
+        
+        if(flag1 && flag2)
+            return nums[mid];
+        else{
+            int x= search(l,mid-1,nums);
+            int y= search(mid+1,h,nums);
+            if(x!=-1)
+                return x;
+            else
+                return y;
+        }
+    }
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        if(nums.size()==1){
+            return nums[0];
+        }
+        return search(0,nums.size()-1,nums);
+    }
+};
