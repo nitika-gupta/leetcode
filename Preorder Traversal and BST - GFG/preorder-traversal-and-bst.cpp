c038b52e8c464dc4758209b58d5e175a46b51a4f
nitika-gupta <ngnitika1@gmail.com>
2022-07-13 12:35:12 +0530

    Added solution - LeetHub

--- Preorder Traversal and BST - GFG/preorder-traversal-and-bst.cpp
+++ Preorder Traversal and BST - GFG/preorder-traversal-and-bst.cpp
@@ -9,44 +9,45 @@ using namespace std;
 
 class Solution {
   public:
-  
-    // void solve(int arr[],int n,int lb,int ub,int &i){
-    //     if(i>=n || arr[i]<lb || arr[i]>ub){
-    //         return;
-    //     }
-    //     //cout<<i<<" ";
-    //     int lim = arr[i];
-    //     i++;
-        
-    //     solve(arr,n, lb,lim,i);
-    //     solve(arr,n,lim,ub,i);
-    //     return;
-    // }
-    // int canRepresentBST(int arr[], int N) {
-    //     // code here
-    //     int i=0;
-    //     solve(arr,N,INT_MIN,INT_MAX,i);
-    //     if(i>=N)
-    //         return 1;
-    //     return 0;
-    // }
+    
     int i=0;
-    void f(int lo,int hi,int pre[], int N){
-        if(i>=N || pre[i]>hi || pre[i]<lo) return ;
+    void solve(int arr[],int n,int lb,int ub){
+        if(i>=n || arr[i]<lb || arr[i]>ub){
+            return;
+        }
+        //cout<<i<<" ";
+        int lim = arr[i];
+        i++;
         
-        //virtually assing value to node and incrementing i
-        int lim=pre[i++];
-        //left child call
-        f(lo,lim,pre,N);
-        //right child call
-        f(lim,hi,pre,N);
+        solve(arr,n, lb,lim);
+        solve(arr,n,lim,ub);
+        return;
     }
     int canRepresentBST(int arr[], int N) {
-        f(INT_MIN,INT_MAX,arr,N);
-        //i has reached the end hence all numbers in the array can be assigned as nodes
-        if(i>=N) return 1;
+        // code here
+        //int i=0;
+        solve(arr,N,INT_MIN,INT_MAX);
+        if(i>=N)
+            return 1;
         return 0;
     }
+    // int i=0;
+    // void f(int lo,int hi,int pre[], int N){
+    //     if(i>=N || pre[i]>hi || pre[i]<lo) return ;
+        
+    //     //virtually assing value to node and incrementing i
+    //     int lim=pre[i++];
+    //     //left child call
+    //     f(lo,lim,pre,N);
+    //     //right child call
+    //     f(lim,hi,pre,N);
+    // }
+    // int canRepresentBST(int arr[], int N) {
+    //     f(INT_MIN,INT_MAX,arr,N);
+    //     //i has reached the end hence all numbers in the array can be assigned as nodes
+    //     if(i>=N) return 1;
+    //     return 0;
+    // }
 };
 
 // { Driver Code Starts.
