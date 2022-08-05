1b28ab676b50be307d2d79272aeaf69986c9e6ea
nitika-gupta <ngnitika1@gmail.com>
2022-08-05 22:12:23 +0530

    Time: 149 ms (52.14%), Space: 19.9 MB (92.15%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,14 +7,22 @@ public:
         vector<vector<int>> res;
          map<vector<int>,int> mp; 
         for(int i=0;i<nums.size()-2;i++){
-          
+            
+            if(i>0){
+                if(nums[i-1]==nums[i])
+                    continue;
+            }
             int j = i+1;
             int k= nums.size()-1;
             
             while(j<k){
                 
                 if(nums[i] + nums[j] + nums[k]==0){
-                    mp[{nums[i],nums[j],nums[k]}]++;
+                    res.push_back({nums[i],nums[j],nums[k]});
+                   // mp[{nums[i],nums[j],nums[k]}]++;
+                    
+                    while(j+1<k && nums[j] == nums[j+1])j++;
+                    while(j<k-1 && nums[k] == nums[k-1])k--;
                     j++;
                     k--;
                 }
@@ -26,9 +34,9 @@ public:
                 }
             }
         }
-        for(auto it: mp){
-            res.push_back(it.first);
-        }
+        // for(auto it: mp){
+        //     res.push_back(it.first);
+        // }
         return res;
     }
 };
